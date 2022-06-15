28fa86589eaeedea3b4f589e452b9b1d9f2165da
Jorge <jorge.carpena@tecsup.edu.pe>
2022-06-14 21:17:10 -0500

    Agregar archivos de proyecto.

---
+++ Infraestructure/Migrations/202206130757322_v3.cs
@@ -0,0 +1,20 @@
+﻿namespace Infraestructure.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class v3 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Students", "studentAddress", c => c.String(nullable: false));
+            DropColumn("dbo.Students", "studentAdress");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Students", "studentAdress", c => c.String(nullable: false));
+            DropColumn("dbo.Students", "studentAddress");
+        }
+    }
+}
---
+++ MVCAJAX12/Controllers/StudentController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Domain;
+using Service;
+using MVCAJAX12.Models;
+
+namespace MVCAJAX12.Controllers
+{
+    public class StudentController : Controller
+    {
+        private StudentService service = new StudentService();
+
+        //GET: Student
+        public ActionResult IndexRazor()
+        {
+            var model = (from c in service.Get()
+                         select new StudentModel
+                         {
+                             ID = c.studentID,
+                             Code = c.studentCode,
+                             Name = c.studentName,
+                             LastName = c.studentLastName,
+                             Address = c.studentAddress
+                         }).ToList();
+
+            return View(model);
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult getStudent(string id)
+        {
+            return Json(service.Get(), JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult createStudent(Student std)
+        {
+            service.Insert(std);
+            string message = "SUCCESS";
+            return Json(new { Message = message, JsonRequestBehavior.AllowGet });
+        }
+    }
+}
